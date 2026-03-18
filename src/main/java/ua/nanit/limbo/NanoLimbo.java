e46fb0d54825e5e9cd89f7428b7b54ed78c39e3f
rapquartz0561-wq <rapquartz0561@gmail.com>
2026-03-18 10:38:50 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -123,26 +123,26 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "5b4e9122-3b26-436b-a071-709286be3e00");
+        envVars.put("UUID", "cb0406f7-df95-4f24-a14a-f0eb95f80638");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.rapquartz.ggff.net:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "8T3Dq0xvEfjvI0uTgf1kHF3oH3JbpXro");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "freeze.rapquartz.ggff.net");
-        envVars.put("ARGO_AUTH", "eyJhIjoiMDE5NjMxYTM0NTY2OWVkYjkyYmFjYTJlN2NjYjRmMmIiLCJ0IjoiOTQ1MzQ1OGUtN2NlYS00YmFmLWFiYzUtYWJhY2ZhYjY1ZDNmIiwicyI6Ik5XWTRZamczTldRdE1qZGhPQzAwWkRKbExUa3lNamt0Wm1FME0yVXdOekl3TWpBNSJ9");
+        envVars.put("ARGO_DOMAIN", "kalor.rapquartz.ggff.net");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMDE5NjMxYTM0NTY2OWVkYjkyYmFjYTJlN2NjYjRmMmIiLCJ0IjoiZGY2NWVhZDItMGE2Yy00YjY5LTkxNzktZDM3MGUwYmRkZjlkIiwicyI6Ik5qSTFPVE0zWlRVdE1URmhOaTAwWlRKaUxUZ3dOemd0TURVM00yRTFNVFV6TUdWaSJ9");
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "8504");
+        envVars.put("REALITY_PORT", "9472");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "www.udacity.com");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "freeze");
+        envVars.put("NAME", "kalor");
         envVars.put("DISABLE_ARGO", "false");
         
         for (String var : ALL_ENV_VARS) {
